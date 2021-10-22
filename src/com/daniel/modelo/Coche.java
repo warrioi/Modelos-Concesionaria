48560adf4005e02717a26259f925f55b757c36c4
unknown <rojasdani248@gmail.com>
2021-10-22 11:12:31 -0400

    consesionaria

---
+++ src/com/daniel/modelo/Coche.java
@@ -0,0 +1,81 @@
+package com.daniel.modelo;
+
+public class Coche {
+    //PK
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private String color;
+    private double precio;
+    //Fk
+    private Cliente cliente;
+
+    public Coche(String matricula, String marca, String modelo, String color, double precio, Cliente cliente) {
+        this.setMatricula(matricula);
+        this.setMarca(marca);
+        this.setModelo(modelo);
+        this.setColor(color);
+        this.setPrecio(precio);
+        this.setCliente(cliente);
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Coche{" +
+                "matricula='" + matricula + '\'' +
+                ", marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", color='" + color + '\'' +
+                ", precio=" + precio +
+                ", cliente=" + cliente +
+                '}';
+    }
+}
